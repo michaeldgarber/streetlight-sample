5abf3b45d0c27668c14c4056738432f2c8650224
michaeldgarber <mdg711@gmail.com>
2022-07-14 14:33:54 -0600

    add definition of look-up tables - code that is sourced

---
+++ scripts/define-urls-look-up-tables.R
@@ -0,0 +1,6 @@
+#Define url objects to link to github-hosted convenience look-up tables
+#Created July 14, 2022
+
+us_state_fips_url = "https://raw.githubusercontent.com/michaeldgarber/lookups-county-state/main/us-state-fips.csv"
+county_urban_rural_2013_url = "https://raw.githubusercontent.com/michaeldgarber/lookups-county-state/main/nchs-urban-rural-2013.csv"
+region_division_url = "https://raw.githubusercontent.com/michaeldgarber/lookups-county-state/main/division-region.csv"
